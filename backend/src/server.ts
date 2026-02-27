4e43421c476c51dab448b760a2771ff442fc2c36
AbdElrahman <abdoxawd@gmail.com>
2026-02-27 02:53:37 +0200

    chore:db connection with TypeORM

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import { app } from "./app";
+import { AppDataSource } from "./config/data-source";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
