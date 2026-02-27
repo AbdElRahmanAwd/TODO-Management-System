2b99805dee3be841b65c7c7b34b425bb7f8bbdfd
AbdElrahman <abdoxawd@gmail.com>
2026-02-27 20:12:41 +0200

    fix:CORS domain

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import todoRouter from "./routes/todo.route";
 
 export const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
 
 app.get("/health", (req, res) => {
   res.send("Hello, World!");
